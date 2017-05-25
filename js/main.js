c72939b2402f2d44483a72255ba1ca56dfbb0808
Graeme Herbert <graeme.redbaobab@gmail.com>
2017-05-25 14:12:33 +0200

    Legend title

--- js/main.js
+++ js/main.js
@@ -115,8 +115,9 @@ $(document).ready(function(){
 
 					var div = L.DomUtil.create('div', 'info legend'),
 					    grades = [1, 7, 10, 13, 16, 19, 22],
-					    div.innerHTML ='<strong>Precio del alquiler por m²]</strong><br>';
+					    title ='<strong>Precio del alquiler por m²]</strong><br>';
 
+					 div.innerHTML += title
 					// loop through our density intervals and generate a label with a colored square for each interval
 					for (var i = 0; i < grades.length; i++) {
 							    div.innerHTML +=
